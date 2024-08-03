1ec7e7289cc9b2cc11c4fcfc3b0dbcd709f515bd
umeki-special <manmosukowai@gmail.com>
2024-08-03 14:45:04 +0900

    0803/03

--- app/controllers/index_controller.rb
+++
@@ -1,5 +0,0 @@
-class IndexController < ApplicationController
-  def index
-    @index = Index.all
-  end
-end
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,5 @@
+class ItemsController < ApplicationController
+  def index
+    # 必要に応じてインスタンス変数などを定義
+  end
+end
