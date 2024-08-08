5e6ea8e115ab382bbc7a16fce625824d4d8f9b79
umeki-special <manmosukowai@gmail.com>
2024-08-08 18:19:54 +0900

    0808単体テスト前

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   validates :nickname, presence: true
+  validates :password_confirmation, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 
 end
