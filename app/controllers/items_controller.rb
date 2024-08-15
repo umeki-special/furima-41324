6d9f690781004233f963736f31785031889fafb1
umeki-special <manmosukowai@gmail.com>
2024-08-15 19:36:48 +0900

    0815 コードレビュー前

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,15 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @item = Item.new
   end
 
   def show
-    # @item = Item.find_by(id: params[:id])
-    # unless @item
-      # redirect_to items_path, alert: "Item not found"
-    # end
+    @item = Item.find_by(id: params[:id])
+    unless @item
+      redirect_to items_path, alert: "Item not found"
+    end
   end
 
   def create
@@ -21,13 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def index
-    # @item = Item.new@items = Item.all
+    @items = Item.order(created_at: :desc)  # 新しい順にソート
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description_of_item, :price, :category_id, :status_id, :shipping_method_id, :region_of_origin_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
-
+    params.require(:item).permit(:name, :description_of_item, :price, :content, :image, :category_id, :status_id, :shipping_method_id, :region_of_origin_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
   end
 end
