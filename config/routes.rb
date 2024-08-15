3c151f6a01dc13be50eda0a2fc8574850aaf6d50
umeki-special <manmosukowai@gmail.com>
2024-08-15 21:29:47 +0900

    0815 商品詳細表示機能コードレビュー前

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :show, :new, :create]
+  resources :items, only: [:index, :show, :new, :create] do
+    member do
+      get 'purchase'
+    end
+  end
   root to: 'items#index'
 end
