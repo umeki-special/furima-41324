3fea339fccf5dc7c43cbc386441c2be5a8efa9ca
umeki-special <manmosukowai@gmail.com>
2024-08-16 15:28:02 +0900

    0816  商品削除コードレビュー前

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-    # member do
-      # get 'purchase'  # or post 'purchase' depending on how you handle the purchase
-    # end
+  resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+    member do
+      get 'purchase'  # or post 'purchase' depending on how you handle the purchase
+    end
   end
   root to: 'items#index'
 end
