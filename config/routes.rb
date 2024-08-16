938b94b02ef23df05e40241f1c01db2166037372
umeki-special <manmosukowai@gmail.com>
2024-08-16 16:12:35 +0900

    ～specファイルを作成しましょう
    https://master.tech-camp.in/v2/curriculums/8127#5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-    #member do
-      #get 'purchase'  # or post 'purchase' depending on how you handle the purchase
-   # end
+    member do
+      get 'purchase'  # または post 'purchase' など購入機能に応じて
+    end
   end
+
+  resources :orders, only: [:create]
+
+  # トップページのルート設定
   root to: 'items#index'
 end
