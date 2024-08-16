93a73ca4390a05a8eb635c5f2843c0f5764e6650
umeki-special <manmosukowai@gmail.com>
2024-08-16 15:45:06 +0900

    0816 商品削除修正02

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  #resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+  resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
     #member do
       #get 'purchase'  # or post 'purchase' depending on how you handle the purchase
    # end
