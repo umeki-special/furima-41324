9f4c9d767c59a68dad984520857f7935768f9ff0
umeki-special <manmosukowai@gmail.com>
2024-08-22 20:36:52 +0900

    0822 商品購入機能修正05

--- app/models/order.rb
+++
@@ -1,6 +0,0 @@
-# app/models/order.rb
-class Order < ApplicationRecord
-  has_one :history
-
-  validates :price, :item, :user, presence: true
-end
--- spec/models/donation_address_spec.rb
+++ spec/models/donation_address_spec.rb
@@ -90,6 +90,18 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Token is invalid")
       end
+
+      it 'userが紐付いていなければ保存できないこと' do
+        @order_form.user_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'itemが紐付いていなければ保存できないこと' do
+        @order_form.item_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
