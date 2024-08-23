bff89577db059527c2aab04651e3a331a3b3ccc7
umeki-special <manmosukowai@gmail.com>
2024-08-23 13:28:19 +0900

    0823

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -14,6 +14,24 @@ class OrderForm
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "must be selected" }
 
+  def create
+    @order_form = OrderForm.new(order_form_params)
+    if @order_form.valid?
+      pay_item
+      if @order_form.save
+        redirect_to root_path
+      else
+        gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+        render 'index', status: :unprocessable_entity
+      end
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
+    end
+  end
+  
+
+
   def save
     history = History.create(item_id: item_id, user_id: user_id)
     Destination.create(
